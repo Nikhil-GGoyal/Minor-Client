ebd3f98a2ced148d43a74af170ff69994ce20c28
MashoorNEGI <ayushdeepnegi@gmail.com>
2022-10-29 17:53:37 +0530

    sillymistakes

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,24 +1,35 @@
-import "./Login.scss"
-function Login() {
-    return (
-        <div className="Login-div">
-            <form className="Login-form" action="">
-                <img src="https://cdn-icons-png.flaticon.com/512/3024/3024605.png" />
-                <div className="L-I-div">
-                    <label>Faculty ID : </label>
-                    <input className="Login-input" type="text" name="" id="" />
-                </div>
-                <div className="L-I-div">
-                    <label>Faculty ID : </label>
-                    <input className="Login-input" type="text" name="" id="" />
-                </div>
-
-                <a className="Login-btn" href="/attendace">Login</a>
-                <div className="Login-r-btn">
-                    Don't have account ?  <a href="/register">Register</a>
-                </div>
-            </form>
+import "./login.scss"
+function login()
+{
+  return (
+    <div class="container">
+        <div class="text">
+            <h1>Log in</h1>
+            <br/>
+            <h3>Mark the attendance in a digital way!</h3>
         </div>
-    );
+        <hr/>
+        <div class="your-input">
+            <div class="input">
+                <input type="text" name="email" id="email" required />
+                <label for="email">Email</label>
+            </div>
+            <div class="input">
+                <input type="text" name="facultyID" id="facID" required />
+                <label for="facID">Faculty ID</label>
+            </div>
+            <div class="input">
+                <input type="password" name="password" id="password" required />
+                <label for=" password">
+                    Password
+                </label>
+            </div>
+        </div>
+        <a href="#" class="forgot-password-link">
+            Forgot Password?
+        </a>
+        <button>Sign in</button>
+    </div>
+);
 }
-export default Login;
+export default login;
